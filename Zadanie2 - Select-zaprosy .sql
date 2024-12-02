3e5a2bace768d6ceca3636b9721cfc80a5401963
mitrij2005 <mitrij2005@mail.ru>
2024-12-03 02:45:26 +0300

    modified:   Zadanie2 - Select-zaprosy .sql 	modified:   Zadanie3 - Select-zaprosy .sql

--- Zadanie2 - Select-zaprosy .sql
+++ Zadanie2 - Select-zaprosy .sql
@@ -1,9 +1,14 @@
 
-# 1. Название и продолжительность самого длительного трека.
-SELECT MAX (nazvanie_treka), MAX(dlitelnost) FROM treki;
+-- 1. Название и продолжительность самого длительного трека.
+--SELECT MAX (nazvanie_treka), MAX(dlitelnost) FROM treki;
 
-# 2. Название треков, продолжительность которых не менее 3,5 минут.
-SELECT treki.nazvanie_treka FROM treki WHERE dlitelnost < 210;
+SELECT treki.nazvanie_treka, treki.dlitelnost FROM treki
+ORDER BY treki.dlitelnost DESC
+LIMIT 1;
+
+-- 2. Название треков, продолжительность которых не менее 3,5 минут.
+--SELECT treki.nazvanie_treka FROM treki WHERE dlitelnost < 210;
+SELECT treki.nazvanie_treka FROM treki WHERE dlitelnost >= 210;
 
 # 3. Названия сборников, вышедших в период с 2018 по 2020 год включительно.
 SELECT sborniki.nazvanie_sbornika, sborniki.god_vypuska FROM sborniki WHERE god_vypuska >= 2018 AND god_vypuska <= 2020;
@@ -12,6 +17,12 @@ SELECT sborniki.nazvanie_sbornika, sborniki.god_vypuska FROM sborniki WHERE god_
 SELECT ispolniteli.imya_ispolnitelya FROM ispolniteli WHERE POSITION(' ' IN ispolniteli.imya_ispolnitelya) = 0;
 
 # 5. Название треков, которые содержат слово «мой» или «my»
+--SELECT treki.nazvanie_treka FROM treki 
+--WHERE UPPER(treki.nazvanie_treka) LIKE '% MY %' OR UPPER(treki.nazvanie_treka) LIKE 'MY %' OR UPPER(treki.nazvanie_treka) LIKE '% MY' OR
+--      UPPER(treki.nazvanie_treka) LIKE '% МОЙ %' OR UPPER(treki.nazvanie_treka) LIKE 'МОЙ %' OR UPPER(treki.nazvanie_treka) LIKE '% МОЙ' OR; 
+
 SELECT treki.nazvanie_treka FROM treki 
 WHERE UPPER(treki.nazvanie_treka) LIKE '% MY %' OR UPPER(treki.nazvanie_treka) LIKE 'MY %' OR UPPER(treki.nazvanie_treka) LIKE '% MY' OR
-      UPPER(treki.nazvanie_treka) LIKE '% МОЙ %' OR UPPER(treki.nazvanie_treka) LIKE 'МОЙ %' OR UPPER(treki.nazvanie_treka) LIKE '% МОЙ' ; 
+      UPPER(treki.nazvanie_treka) LIKE '% МОЙ %' OR UPPER(treki.nazvanie_treka) LIKE 'МОЙ %' OR UPPER(treki.nazvanie_treka) LIKE '% МОЙ' OR
+      UPPER(treki.nazvanie_treka) = 'MY' OR
+      UPPER(treki.nazvanie_treka) = 'МОЙ'; 
--- Zadanie3 - Select-zaprosy .sql
+++ Zadanie3 - Select-zaprosy .sql
@@ -1,40 +1,58 @@
  
-SELECT ispolnitely_ganry.id_ispolnitel_ganr, ispolnitely_ganry.id_ganra, ispolnitely_ganry.id_ispolnitelya, ganry.nazvanie_ganra, COUNT (*) FROM ispolnitely_ganry
+/*SELECT ispolnitely_ganry.id_ispolnitel_ganr, ispolnitely_ganry.id_ganra, ispolnitely_ganry.id_ispolnitelya, ganry.nazvanie_ganra, COUNT (*) FROM ispolnitely_ganry
 JOIN ganry ON ganry.id_ganra = ispolnitely_ganry.id_ganra
 GROUP BY ispolnitely_ganry.id_ganra, ispolnitely_ganry.id_ispolnitelya, ispolnitely_ganry.id_ispolnitel_ganr, ganry.nazvanie_ganra
-ORDER BY ispolnitely_ganry.id_ganra;
+ORDER BY ispolnitely_ganry.id_ganra;*/
 
-SELECT ganry.id_ganra, ganry.nazvanie_ganra FROM ganry
+/*SELECT ganry.id_ganra, ganry.nazvanie_ganra FROM ganry
  RIGHT  JOIN ispolnitely_ganry ON ispolnitely_ganry.id_ganra = ganry.id_ganra
  WHERE ispolnitely_ganry.id_ganra.KEY IS NULL; 
-#  RIGHT JOIN ispolniteli ON ispolniteli.id_ispolnitelya = ispolnitely_ganry.id_ispolnitelya;
+#  RIGHT JOIN ispolniteli ON ispolniteli.id_ispolnitelya = ispolnitely_ganry.id_ispolnitelya; */
 
-# 1.Количество исполнителей в каждом жанре
+-- 1.Количество исполнителей в каждом жанре
 SELECT ganry.id_ganra, ganry.nazvanie_ganra, COUNT(ispolnitely_ganry.id_ganra) FROM ganry
   LEFT JOIN ispolnitely_ganry ON ispolnitely_ganry.id_ganra = ganry.id_ganra
   GROUP BY ganry.id_ganra ;
 
-# 2. Количество треков, вошедших в альбомы 2019–2020 годов
-SELECT albomy.id_alboma, albomy.nazvanie_alboma, albomy.god_vypuska, COUNT (treki.id_treka) FROM albomy 
+-- 2. Количество треков, вошедших в альбомы 2019–2020 годов
+/*SELECT albomy.id_alboma, albomy.nazvanie_alboma, albomy.god_vypuska, COUNT (treki.id_treka) FROM albomy 
 JOIN treki ON treki.id_alboma = albomy.id_alboma
 WHERE albomy.god_vypuska >= 2020 AND albomy.god_vypuska <= 2022
-GROUP BY albomy.id_alboma;
+GROUP BY albomy.id_alboma;*/
 
-# 3. Средняя продолжительность треков по каждому альбому.
+SELECT  COUNT (treki.id_treka) FROM treki 
+JOIN albomy ON treki.id_alboma = albomy.id_alboma
+WHERE albomy.god_vypuska BETWEEN 2020 AND 2022;
+
+
+-- 3. Средняя продолжительность треков по каждому альбому.
 SELECT albomy.id_alboma, albomy.nazvanie_alboma, albomy.god_vypuska, AVG(treki.dlitelnost) FROM albomy 
 JOIN treki ON treki.id_alboma = albomy.id_alboma
 GROUP BY albomy.id_alboma;
 
 
-# 4. Все исполнители, которые не выпустили альбомы в 2020 году.
-SELECT ispolniteli.id_ispolnitelya, ispolniteli.imya_ispolnitelya, ispolnitely_albomy.id_alboma, albomy.nazvanie_alboma, albomy.god_vypuska FROM ispolniteli
-JOIN ispolnitely_albomy ON ispolnitely_albomy.id_ispolnitelya = ispolniteli.id_ispolnitelya
-JOIN albomy ON albomy.id_alboma = ispolnitely_albomy.id_alboma
-WHERE albomy.god_vypuska <> 2020
-GROUP BY ispolniteli.id_ispolnitelya UNIQUE;
 
-# 4. Все исполнители, которые не выпустили альбомы в 2020 году (исключены повторения). 
+-- 4. Все исполнители, которые не выпустили альбомы в 2020 году (исключены повторения). 
+/*
 SELECT DISTINCT ispolniteli.id_ispolnitelya, ispolniteli.imya_ispolnitelya FROM ispolniteli
 JOIN ispolnitely_albomy ON ispolnitely_albomy.id_ispolnitelya = ispolniteli.id_ispolnitelya
 JOIN albomy ON albomy.id_alboma = ispolnitely_albomy.id_alboma
 WHERE albomy.god_vypuska <> 2020;
+*/
+SELECT ispolniteli.imya_ispolnitelya
+FROM ispolniteli
+WHERE ispolniteli.imya_ispolnitelya NOT IN (
+    SELECT ispolniteli.id_ispolnitelya, ispolniteli.imya_ispolnitelya FROM ispolniteli
+    JOIN ispolnitely_albomy ON ispolnitely_albomy.id_ispolnitelya = ispolniteli.id_ispolnitelya
+    JOIN albomy ON albomy.id_alboma = ispolnitely_albomy.id_alboma
+    WHERE albomy.god_vypuska = 2020
+);
+
+-- 5.Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
+SELECT DISTINCT sborniki.nazvanie_sbornika FROM sborniki
+JOIN treki_sbornika ON treki_sbornika.id_sbornika = sborniki.id_sbornika
+JOIN treki ON treki.id_treka = treki_sbornika.id_treka
+JOIN albomy ON albomy.id_alboma = treki.id_alboma
+JOIN ispolnitely_albomy ON ispolnitely_albomy.id_alboma = albomy.id_alboma
+JOIN ispolniteli ON ispolniteli.id_ispolnitelya = ispolnitely_albomy.id_ispolnitelya
+WHERE ispolniteli.imya_ispolnitelya = 'The Doors';
